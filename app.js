8ae6e6143d680dce3bf401a9c910e09334e6678f
Muhammad Hanzala <mhanzala989@gmail.com>
2017-05-19 19:02:14 +0500

    incomplete

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const routes = require('./routes/routes');
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/muber');
+
+app.use(bodyParser.json());
+routes(app);
+
+
+module.exports = app ;
